3adfedba4963c112dcfb9ce8a55fe4e93906952a
Yuuki-kun <tongcongminhart@gmail.com>
2024-05-21 19:14:50 +0700

    first commit

---
+++ LearningCSharp/EmptyClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace LearningCSharp
+{
+	public class EmptyClass
+	{
+		public EmptyClass()
+		{
+			
+		}
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello world");
+		}
+	}
+}
+
